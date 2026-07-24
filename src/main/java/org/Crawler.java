ebcdceee92767273fd863c6785a0e1293d39bbd9
Milos-05 <milosgvozdenovic05@gmail.com>
2026-07-24 21:17:10 +0200

    Added getText method that strips HTML and made other small changes

--- src/main/java/org/Crawler.java
+++ src/main/java/org/Crawler.java
@@ -5,20 +5,18 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Crawler {
-    private final BlockingQueue<String> urlQueue;
+    private final BlockingQueue<Url> urlQueue;
     private final ExecutorService executor;
     private final WikiHttpFetcher fetcher;
     private final Set<String> visited;
     private final RateLimiter rateLimiter;
 
-    private static final int MAX_REQUESTS = 100;
-    private static final double RATE = 200.0/60.0;
+    private static final int MAX_REQUESTS = 1_000_000;
+    private static final double RATE = 3.0;
 
     public Crawler() {
         urlQueue = new LinkedBlockingQueue<>();
@@ -28,30 +26,35 @@ public class Crawler {
         executor = Executors.newVirtualThreadPerTaskExecutor();
     }
     public void start(){
-        urlQueue.add("https://en.wikipedia.org/wiki/Java_(programming_language)");
-        int i=0;
-        while (true) {
-            i++;
+        Url input=new Url("https://en.wikipedia.org/wiki/Java_(programming_language)",null);
+        urlQueue.add(input);
+        visited.add(input.getUrl());
+       for(int i=0;i<MAX_REQUESTS;i++){
             rateLimiter.acquire();
             int finalI = i;
             executor.submit(() -> {
                 try {
-                    String url = urlQueue.poll(5, TimeUnit.SECONDS);
+                    Url url = urlQueue.poll(5, TimeUnit.SECONDS);
                     if(url==null){
                         return;
                     }
+                    Document doc = fetcher.getWikiResponse(url.getUrl());
 
-                    System.out.println( finalI +": Uzeo url: " + url);
-                    Document doc = fetcher.getWikiResponse(url);
                     if (doc == null) {
                         System.err.println("Failed to fetch: " + url);
                         return;
                     }
-
+                    String text=getText(doc);
+                    if(text!=null) {
+                        Main.pageTexts.add(new PageContent(text,url.getUrl()));
+                    }
                     Elements wikiLinks = doc.select("a[rel=mw:WikiLink]");
+                    int count=wikiLinks.size();
+                    System.out.println( finalI +": Added url: " + url+" added "+count+" new urls, and total urls waiting="+urlQueue.size());
                     for (Element element : wikiLinks) {
-                        String newUrl = element.attr("abs:href");
-                        if(visited.add(newUrl)) {
+                        String link = element.attr("abs:href");
+                        if(visited.add(link)) {
+                            Url newUrl =new Url(link, url);
                             urlQueue.add(newUrl);
                         }
                     }
@@ -62,5 +65,10 @@ public class Crawler {
             });
         }
     }
-
+    private String getText(Document document){
+        Element content = document.selectFirst("#mw-content-text");
+        if(content==null)return null;
+        document.select("table.ambox, div.hatnote, div.sbox, span.mw-editsection").remove();
+        return content.text();
+    }
 }
--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -1,12 +1,18 @@
 package org;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
+public static BlockingQueue<PageContent> pageTexts;
 
     public static void main(String[] args) throws InterruptedException {
-
+        pageTexts=new LinkedBlockingQueue<>();
         Crawler crawler=new Crawler();
         crawler.start();
         Thread.sleep(50000);
