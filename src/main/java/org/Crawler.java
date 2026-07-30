7e3ee99c3a1f147e902f9392912a06cca1a65d02
Milos-05 <milosgvozdenovic05@gmail.com>
2026-07-30 09:05:50 +0200

    Refactored Crawler

--- src/main/java/org/Crawler.java
+++ src/main/java/org/Crawler.java
@@ -19,13 +19,13 @@ public class Crawler {
     private static final int MAX_REQUESTS = 1_000_000;
     private static final double RATE = 3.0;
 
-    public Crawler() {
+    public Crawler(PageParser parser) {
         urlQueue = new LinkedBlockingQueue<>();
         fetcher = new WikiHttpFetcher();
         visited = ConcurrentHashMap.newKeySet();
         rateLimiter=RateLimiter.create(RATE);
         executor = Executors.newVirtualThreadPerTaskExecutor();
-        pageParser = new PageParser();
+        pageParser = parser;
     }
     public void start(){
         Url input=new Url("https://en.wikipedia.org/wiki/Java_(programming_language)",null);
@@ -33,44 +33,60 @@ public class Crawler {
         visited.add(input.getUrl());
        for(int i=0;i<MAX_REQUESTS;i++){
             rateLimiter.acquire();
-            int finalI = i;
-            executor.submit(() -> {
-                try {
-                    Url url = urlQueue.poll(5, TimeUnit.SECONDS);
-                    if(url==null){
-                        return;
-                    }
-                    Document doc = fetcher.getWikiResponse(url.getUrl());
 
-                    if (doc == null) {
-                        System.err.println("Failed to fetch: " + url);
-                        return;
-                    }
-                    String text=getText(doc);
-                    if(text!=null) {
-                        pageParser.getPages().add(new PageContent(text,url.getUrl()));
-                    }
-                    Elements wikiLinks = doc.select("a[rel=mw:WikiLink]");
-                    int count=wikiLinks.size();
-                    System.out.println( finalI +": Added url: " + url+" added "+count+" new urls, and total urls waiting="+urlQueue.size());
-                    for (Element element : wikiLinks) {
-                        String link = element.attr("abs:href");
-                        if(visited.add(link)) {
-                            Url newUrl =new Url(link, url);
-                            urlQueue.add(newUrl);
-                        }
-                    }
-
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            });
+            executor.submit(this::processUrl);
         }
     }
+
+    private void processUrl() {
+        try {
+            Url url = urlQueue.poll(5, TimeUnit.SECONDS);
+            if(url==null) return;
+
+            Document doc = fetchDocument(url);
+            if (doc == null) return;
+
+            addDocumentToParser(doc, url);
+
+            getLingsAndAddThemToQueue(doc, url);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void getLingsAndAddThemToQueue(Document doc, Url url) {
+        Elements wikiLinks = doc.select("a[rel=mw:WikiLink]");
+        int count=wikiLinks.size();
+        System.out.println(": Added url: " + url +" added "+count+" new urls, and total urls waiting="+urlQueue.size());
+        for (Element element : wikiLinks) {
+            String link = element.attr("abs:href");
+            if(visited.add(link)) {
+                Url newUrl =new Url(link, url);
+                urlQueue.add(newUrl);
+            }
+        }
+    }
+
+    private Document fetchDocument(Url url) {
+        Document doc = fetcher.getWikiResponse(url.getUrl());
+        if (doc == null) {
+            System.err.println("Failed to fetch: " + url);
+            return null;
+        }
+        return doc;
+    }
+
     private String getText(Document document){
         Element content = document.selectFirst("#mw-content-text");
         if(content==null)return null;
         document.select("table.ambox, div.hatnote, div.sbox, span.mw-editsection").remove();
         return content.text();
+    }
+    private void addDocumentToParser(Document doc,Url url){
+        String text=getText(doc);
+        if(text!=null) {
+            pageParser.addPageToQueue(new PageContent(text,url.getUrl()));
+        }
     }
 }
